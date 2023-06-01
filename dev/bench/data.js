f8635f6e0b7c6138f385401286c33432344f1c46
github-action-benchmark <github@users.noreply.github.com>
2023-06-01 10:02:33 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 24a97938fb8bb90141348b940bd0e8e35dce1a37

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1685613753498,
+  "repoUrl": "https://github.com/ClementWalter/benchmark-action-setup",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "clement0walter@gmail.com",
+            "name": "Clément Walter",
+            "username": "ClementWalter"
+          },
+          "committer": {
+            "email": "clement0walter@gmail.com",
+            "name": "Clément Walter",
+            "username": "ClementWalter"
+          },
+          "distinct": true,
+          "id": "24a97938fb8bb90141348b940bd0e8e35dce1a37",
+          "message": "Initial commit",
+          "timestamp": "2023-06-01T12:02:15+02:00",
+          "tree_id": "6648ca00db8464249f5bee2260458a51e83431e2",
+          "url": "https://github.com/ClementWalter/benchmark-action-setup/commit/24a97938fb8bb90141348b940bd0e8e35dce1a37"
+        },
+        "date": 1685613752489,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "CPU Load",
+            "value": 20,
+            "unit": "Percent"
+          },
+          {
+            "name": "Memory Used",
+            "value": 284,
+            "unit": "Megabytes",
+            "range": "9",
+            "extra": "Value for Tooltip: 41\nOptional Num #2: 151\nAnything Else!"
+          }
+        ]
+      }
+    ]
+  }
+}
